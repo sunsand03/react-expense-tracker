3cbe5aed0c08303cb4eec22d45fdf7b94c4d9637
codiku-dev <codiku-dev@gmail.com>
2022-10-14 18:02:48 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import "./global.css";
 import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
