0d27f0ebd9a79d5c8a1b87ca5f8f2ae627d067db
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-24 19:07:46 +0100

    Update ExpenseInput.jsx

--- src/components/ExpenseInput/ExpenseInput.jsx
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -5,10 +5,11 @@ export function ExpenseInput(props) {
     <form>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
-          <input
+           <input
             type="text"
             className="form-control"
-            placeholder='Ex : "Pomme"'
+            placeholder='Ex : "Apple"'
+            name="name"
           />
         </div>
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
@@ -17,6 +18,7 @@ export function ExpenseInput(props) {
             step="0.01"
             className="form-control"
             placeholder="Ex: 3.99"
+            name="price"
           />
         </div>
 
