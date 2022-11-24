a65e221382d5ad148f5ce4c0c50e371fd6c9ef8f
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-24 17:46:14 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ export function App() {
     <div className={s.main_container}>
       <div className={`row ${s.header}`}>
         <div className={`col-3`}>Logo</div>
-        <div className={`col-9 ${s.income_input}`}>Revenu</div>
+        <div className={`col-9 ${s.income_input}`}>Income input</div>
       </div>
       <div className={`row ${s.workspace}`}>
-        <div className={`col-12  ${s.expense_input}`}>Input des dépenses</div>
+        <div className={`col-12  ${s.expense_input}`}>Expense Input</div>
         <div className={`col-11 col-md-6 col-lg-4 ${s.expense_list}`}>
-          Historique
-          <div className={`col-12 ${s.expense_total}`}>Dépenses total</div>
+          Expense History
+          <div className={`col-12 ${s.expense_total}`}>ExpenseTotal</div>
         </div>
       </div>
     </div>
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,8 +3,8 @@ import s from "./style.module.css";
 export function ListItem({ item }) {
   return (
     <tr>
-      <th>Faux nom</th>
-      <td className={s.price}>Faux prix €</td>
+      <th>Fake name</th>
+      <td className={s.price}>Fake price $</td>
     </tr>
   );
 }
