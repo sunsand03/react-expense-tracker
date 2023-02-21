549462f2aac9381cc0063094811d4ba8d374b313
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-21 13:35:39 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import "./global.css";
 import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux"
-import { store } from "./store/index"
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/index";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    < Provider store={store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { expenseSlice } from "./expense/expense-slice"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { expenseSlice } from "./expense/expense-slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
+// 3 Set a configuration and whitelist for slices reducer you want to persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["EXPENSE"],
+};
+
+// 1 Combine the reducers into a single reducer
+const allReducers = combineReducers({
+  EXPENSE: expenseSlice.reducer,
+});
+
+// 4 Persist the combined reducers using the configurations provided
+const persistedReducers = persistReducer(persistConfig, allReducers);
+
+// 2 Create the store with the persisted reducers
 const store = configureStore({
-    reducer: {
-        EXPENSE: expenseSlice.reducer,
-    }
-})
+  reducer: persistedReducers,
+});
 
-export { store }
+// 5 Persisted store to send to the PersistGate provider
+let persistor = persistStore(store);
+
+// 6 Export the persisted store as well
+export { store, persistor };
