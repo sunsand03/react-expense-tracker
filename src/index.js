d78bde619c9eba699e58eb5fe2f05faae2d27904
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-21 17:33:03 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import "./global.css";
 import { App } from "./App";
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "store";
+import { store, persistor } from "store";
+import { PersistGate } from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} >
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
+import storage from "redux-persist/lib/storage"
+import {
+  persistStore, persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["EXPENSE"]
+}
+const rootReducers = combineReducers({
+  EXPENSE: expenseSlice.reducer,
+})
+
+const persistedReducers = persistReducer(persistConfig, rootReducers)
 
 const store = configureStore({
-  reducer: {
-    EXPENSE: expenseSlice.reducer,
-  },
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export { store };
+const persistor = persistStore(store)
+
+export { store, persistor };
